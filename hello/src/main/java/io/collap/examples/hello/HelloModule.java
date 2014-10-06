6c55b8bca7597e15e4ab45fb391a408f8de5e7c8
Marco Pennekamp <marco.pennekamp96@gmail.com>
2014-10-06 14:51:31 +0200

    Add 'hello' example.

---
+++ hello/src/main/java/io/collap/examples/hello/HelloModule.java
@@ -0,0 +1,112 @@
+package io.collap.examples.hello;
+
+import io.collap.bryg.EnvironmentConfigurator;
+import io.collap.bryg.EnvironmentCreator;
+import io.collap.bryg.ModuleSourceLoader;
+import io.collap.bryg.compiler.Configuration;
+import io.collap.bryg.compiler.resolver.ClassResolver;
+import io.collap.bryg.environment.Environment;
+import io.collap.bryg.loader.SourceLoader;
+import io.collap.bryg.model.GlobalVariableModel;
+import io.collap.controller.Dispatcher;
+import io.collap.controller.ProviderControllerFactory;
+import io.collap.controller.provider.BrygProvider;
+import io.collap.examples.hello.page.Greeting;
+import io.collap.plugin.Module;
+
+import javax.annotation.Nullable;
+
+public class HelloModule extends Module implements BrygProvider, EnvironmentConfigurator {
+
+    /* The version and artifact name are important for the template source loader to find the sources.
+     * The name and version must NOT differ from the values defined in this module's build.gradle. */
+    public static final String VERSION = "0.1";
+    public static final String ARTIFACT_NAME = "examples-hello-name-" + VERSION;
+
+    private Environment bryg;
+
+    /**
+     * The initialize method is called in stage 4 of the plugin initialization process (Refer to the Overview in the wiki).
+     */
+    @Override
+    public void initialize () {
+        /* The bryg environment must be created before it can be used.
+         * Each module has a different environment, to prevent namespace pollution. */
+        bryg = new EnvironmentCreator (collap, this).create ();
+
+        /* The root dispatcher is the dispatcher that corresponds to the base path of collap.
+         * All other dispatchers and controllers are directly or indirectly registered here. */
+        Dispatcher rootDispatcher = collap.getRootDispatcher ();
+        Dispatcher examplesDispatcher = new Dispatcher (collap); // TODO: Only create and register the dispatcher if it doesn't already exist,
+                                                                 // otherwise: get the dispatcher and use it (Requires API change).
+
+        /* We just created an 'examples' dispatcher, which now needs to be registered at the root to be callable. */
+        rootDispatcher.registerDispatcher ("examples", examplesDispatcher);
+
+        /* This line registers a new controller factory with the Greeting controller and the 'hello' name
+         * at the 'examples' dispatcher. The ProviderControllerFactory expects zero or more types that implement
+         * the Provider interface. Since this module class already provides a bryg environment (and
+         * implements the right interfaces), we can just pass 'this'. */
+        examplesDispatcher.registerControllerFactory ("hello",
+                new ProviderControllerFactory (Greeting.class, this));
+    }
+
+    /**
+     * This is called when collap is shut down.
+     *
+     * TODO: Clarify order of destruction.
+     */
+    @Override
+    public void destroy () {
+
+    }
+
+    /**
+     * This method is primarily used to add annotated classes to the Hibernate configuration.
+     * This method is called before the plugin is initialized.
+     */
+    @Override
+    public void configureHibernate (org.hibernate.cfg.Configuration configuration) {
+
+    }
+
+
+    // Override from EnvironmentConfigurator
+
+    /**
+     * This ModuleSourceLoader allows the Environment to find the template files as resources in the cache.
+     */
+    @Override
+    public SourceLoader getSourceLoader () {
+        return new ModuleSourceLoader (this);
+    }
+
+    @Override
+    public void configureConfiguration (Configuration configuration) {
+
+    }
+
+    @Override
+    public void configureClassResolver (ClassResolver classResolver) {
+
+    }
+
+    @Override
+    public void configureGlobalVariableModel (GlobalVariableModel globalVariableModel) {
+
+    }
+
+    @Override
+    public @Nullable String getArtifactName () {
+        return ARTIFACT_NAME;
+    }
+
+
+    // Override from BrygProvider
+
+    @Override
+    public Environment getBryg () {
+        return bryg;
+    }
+
+}
---
+++ hello/src/main/java/io/collap/examples/hello/page/Greeting.java
@@ -0,0 +1,67 @@
+package io.collap.examples.hello.page;
+
+import io.collap.bryg.environment.Environment;
+import io.collap.bryg.model.Model;
+import io.collap.controller.ModuleController;
+import io.collap.controller.communication.Response;
+import io.collap.controller.provider.BrygDependant;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+
+/**
+ * The Greeting controller can be registered at any dispatcher.
+ * The GET request method asks the user for a name.
+ * The POST request method requires the user to send a name. The controller then greets the user.
+ *
+ * Since this controller is a BrygDependant, the setBryg method (declared in the BrygDependant interface)
+ * is called when an instance of this controller is created.
+ */
+public class Greeting extends ModuleController implements BrygDependant {
+
+    private Environment bryg;
+
+    /**
+     * This method asks the user for a name.
+     */
+    @Override
+    public void doGet (Response response) throws IOException {
+        /* For more detailed explanations about these methods, check the doPost method below. */
+
+        Model model = bryg.createModel ();
+
+        bryg.getTemplate ("NameForm").render (response.getContentWriter (), model);
+        bryg.getTemplate ("NameForm_head").render (response.getHeadWriter (), model);
+    }
+
+    /**
+     * This method greets the user, or says that it doesn't know the user if no name is
+     * supplied through a form.
+     */
+    @Override
+    public void doPost (Response response) throws IOException {
+        /* The @Nullable indicates that the name variable may be null. */
+        @Nullable String name = request.getStringParameter ("name");
+
+        /* A model should be created with the Environment's createModel method
+         * to include global variables defined in the module. */
+        Model model = bryg.createModel ();
+
+        /* The name argument is set here.
+         * A variable with the value 'null' and an undefined variable are essentially the same when
+         * it comes to bryg models. Since the name parameter is optional, we can skip the null check here. */
+        model.setVariable ("name", name);
+
+        /* The first time a template is received from the environment, the environment compiles the template JIT.
+         * This means that the first execution takes much longer, but the template is cached after this first compilation
+         * for the rest of the Environment's lifespan. */
+        bryg.getTemplate ("Greeting").render (response.getContentWriter (), model);
+        bryg.getTemplate ("Greeting_head").render (response.getHeadWriter (), model);
+    }
+
+    @Override
+    public void setBryg (Environment environment) {
+        this.bryg = environment;
+    }
+
+}
